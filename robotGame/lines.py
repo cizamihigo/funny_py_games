dd57fc17f7b634824be9b37fe7ee27fe798d50fa
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 14:46:01 +0200

    Turtle lines

--- robotGame/lines.py
+++ robotGame/lines.py
@@ -4,20 +4,20 @@ import turtle as t
 def get_len():
     choice =input("Enter line length (Long, Medium, Short): ")
     if choice == 'long':
-        line_len = 100
+        line_len = 200
     elif choice =='medium':
-        line_len = 50
+        line_len = 100
     else:
-        line_len = 25
+        line_len = 50
     return line_len
 def get_wid():
     choice = input("Enter line width (superthick, thick, thin): ")
     if choice == 'superthick' :
-        line_wdth = 20
+        line_wdth = 40
     elif choice == 'thick':
-        line_wdth = 10
+        line_wdth = 20
     else: 
-        line_wdth = 5
+        line_wdth = 10
     return line_wdth
 
 def inside_window():
@@ -29,14 +29,17 @@ def inside_window():
     inside = leftlimit < x < rightlimit and bottom_limit < y < top_limit
     return inside
 def move(line_len):
-    # pen_colors =['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    pen_colors =['red', 'orange', 'yellow', 'green', 'blue', 'purple']
     # t.pencolor(random.choice(pen_colors))
     t.colormode(255)
     red = random.randint(0,255)
     blue = random.randint(0,255)
     green = random.randint(0,255)
 
-    t.pencolor(red, blue, green)
+    t.pencolor(red, green, blue)
+    t.fillcolor(random.choice(pen_colors))
+    t.shapesize(3,3,1)
+    t.stamp()
     if inside_window():
         angle = random.randint(0,180)
         t.right(angle)
