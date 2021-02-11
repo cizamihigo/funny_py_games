c1cb34cadf46d5ee924216e03506f47a450ac2a6
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 12:35:23 +0200

    stars in the night hanging the sky

--- robotGame/mind-boggling.py
+++ robotGame/mind-boggling.py
@@ -5,7 +5,7 @@ import random
 #  set the speed, background color, and pen size for the turtle
 colors = cycle(['red','blue','yellow','green', 'purple','light blue', 'thistle','hot pink', 'goldenrod','seashell','lawn green', 'peru','gold','maroon','navy','aquamarine','lemon chiffon', 'peach puff', 'deep pink', 'misty rose'])
 def draw_circles(size,angle,shift,shape):
-    t.bgcolor(next(colors))
+    # t.bgcolor(next(colors))
     next_shape =''
     if shape == 'circle':
         t.circle(size)
@@ -14,7 +14,7 @@ def draw_circles(size,angle,shift,shape):
         next_shape ='square'
     elif shape == 'square':
         for count in range(0,4):
-            t.forward(size +2 )
+            t.forward(size + 7 )
             t.left(90)
         next_shape ='circle'
         t.pencolor(next(colors))
@@ -30,4 +30,4 @@ t.speed('fast')
 
 t.pensize(4)
 
-draw_circles(5,0,1,'circle')
+draw_circles(5,0,1,'square')
--- robotGame/readme.md
+++ robotGame/readme.md
@@ -4,3 +4,4 @@
 **mind-boggling.py** have you used windows XP??? 
 when the PC is spleeping there are such images which were designed on the desktop always starting by simple cercles and appending new cercles one aside others, other inside others and so on till it become complex. the more the program is running the more complicated it becomes.
 >>> EACH NEW CIRCLE IS DIFFERENT FROM THE LAST ONE DRAWN, MAKING THE PATTERN MORE INTERESTING.
+**Night.py** : The night you dreamed about. stars, in the sky, lightning just for you in the night
