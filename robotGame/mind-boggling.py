04ae45c7acaa69ef28727b8ec609d183eaa4ee87
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 12:02:27 +0200

    Working smoothly WinXp Sleeping screen

--- robotGame/mind-boggling.py
+++ robotGame/mind-boggling.py
@@ -4,12 +4,14 @@ from itertools import cycle
 import random
 #  set the speed, background color, and pen size for the turtle
 colors = cycle(['red','blue','yellow','green', 'purple','light blue', 'thistle','hot pink', 'goldenrod','seashell','lawn green', 'peru','gold','maroon','navy','aquamarine','lemon chiffon', 'peach puff', 'deep pink', 'misty rose'])
-def draw_circles(size):
+def draw_circles(size,angle,shift):
     t.pencolor(next(colors))
     t.circle(size)
-    draw_circles(size + 6)
+    t.right(angle)
+    t.forward(shift)
+    draw_circles(size + 5 , angle + 1, shift + 1)
 t.speed('fastest')
 t.bgcolor('black')
 t.pensize(1)
 
-draw_circles(6)
+draw_circles(5,0,1)
