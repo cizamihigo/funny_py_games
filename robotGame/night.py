bae00b029a925d7105cf69e692e9da10b4faa5bb
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 13:45:38 +0200

    WORKING STARS

--- robotGame/night.py
+++ robotGame/night.py
@@ -25,15 +25,16 @@ def draw_stars(points, size, col, x,y):
     t.end_fill()
 
 # main page code
-t.hideturtle()
+# t.hideturtle()
 t.Screen().title("night sky")
 
 t.Screen().bgcolor('dark blue')
 while True:
     ranPt = randint(2,5) * 2 + 1
     ransize = randint(10,50)
-    rancol =(random(),random(),random())
+    rancol =(random(),random(),random()) 
     ranX = randint(-350,300)
     ranY = randint(-250, 250)
-draw_stars(ranPt, ransize, rancol, ranX, ranY)
+    
+    draw_stars(ranPt, ransize, rancol, ranX, ranY)
 # t.Screen().mainloop()
