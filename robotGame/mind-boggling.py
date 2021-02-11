62b5649e42655b482204ad66feca68f49f741eea
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 11:57:05 +0200

    illusion using turtle in python

--- robotGame/mind-boggling.py
+++ robotGame/mind-boggling.py
@@ -4,14 +4,12 @@ from itertools import cycle
 import random
 #  set the speed, background color, and pen size for the turtle
 colors = cycle(['red','blue','yellow','green', 'purple','light blue', 'thistle','hot pink', 'goldenrod','seashell','lawn green', 'peru','gold','maroon','navy','aquamarine','lemon chiffon', 'peach puff', 'deep pink', 'misty rose'])
-t.speed('fast')
-t.bgcolor('black')
-t.pensize(1)
-
-
 def draw_circles(size):
     t.pencolor(next(colors))
     t.circle(size)
-    draw_circles(size)
+    draw_circles(size + 6)
+t.speed('fastest')
+t.bgcolor('black')
+t.pensize(1)
 
-draw_circles(int(input("Enter the initial size:")))
+draw_circles(6)
