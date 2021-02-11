00b8c32b07c12ba18ee2737184f195281c4f961c
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 11:51:41 +0200

    adding colors using itertolls cylcle

--- robotGame/mind-boggling.py
+++ robotGame/mind-boggling.py
@@ -1,14 +1,16 @@
 # Start
 import turtle as t
 from itertools import cycle
+import random
 #  set the speed, background color, and pen size for the turtle
+colors = cycle(['red','blue','yellow','green', 'purple','light blue', 'thistle','hot pink', 'goldenrod','seashell','lawn green', 'peru','gold','maroon','navy','aquamarine','lemon chiffon', 'peach puff', 'deep pink', 'misty rose'])
 t.speed('fast')
 t.bgcolor('black')
 t.pensize(1)
 
 
 def draw_circles(size):
-    t.pencolor('red')
+    t.pencolor(next(colors))
     t.circle(size)
     draw_circles(size)
 
