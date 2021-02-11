04b14d5b7061b496c24c086efc313aa38feb6a9b
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 20:15:35 +0200

    count Down first results

--- CountDown/cdown.py
+++ CountDown/cdown.py
@@ -1,15 +1,6 @@
 from tkinter import Tk, Canvas
 from datetime import date, datetime
 def get_events():
-    # list_events= []
-    # with open('CountDown/events.txt') as file:
-    #     for line in file:
-    #         line = line.rstrip('\n')
-    #         current_event = line.split(',') 
-    #         print(current_event)
-    #         event_date = datetime.strptime(current_event[0], "%d/%m/%y").date()
-    #         current_event[0] = event_date
-    #         list_events.append(current_event)
     list_events = []
     with open("CountDown/events.txt") as file:
         for line in file:
@@ -34,15 +25,15 @@ c.create_text(200, 50, anchor = 'w', fill='orange', \
 c.create_text(250,680, anchor = 'nw', fill = 'white' \
 ,font = 'Cambria 18 bold', text = "Made by @taketherisk11")
 get_events()
-# events = '1/01/2020' # get_events()
-# today =date.today()
+events =  get_events()
+today =date.today()
 
-# for event in events:
-#     event_name = event[0]
-#     day_till = date2date(event[0],today)
-#     display = 'It is %s days unitl %s' %(day_till, event_name)
-#     c.create_text(100,100,anchor='west', fill= 'light blue',\
-#         font = 'Times New Roman 19 bold', text = display)
+for event in events:
+    event_name = event[0]
+    day_till = date2date(event[0],today)
+    display = 'It is %s days unitl %s' %(day_till, event_name)
+    c.create_text(100,100,anchor='w', fill= 'light blue',\
+        font = 'Arial 19 bold', text = display)
 
 
 
