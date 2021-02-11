bf58f3d07c010d502d3e51f3c3d3d479003f7358
cizamihigo <cizamihigochristian@gmail.com>
2021-02-11 12:06:41 +0200

    funny changing color screen

--- robotGame/mind-boggling.py
+++ robotGame/mind-boggling.py
@@ -5,13 +5,14 @@ import random
 #  set the speed, background color, and pen size for the turtle
 colors = cycle(['red','blue','yellow','green', 'purple','light blue', 'thistle','hot pink', 'goldenrod','seashell','lawn green', 'peru','gold','maroon','navy','aquamarine','lemon chiffon', 'peach puff', 'deep pink', 'misty rose'])
 def draw_circles(size,angle,shift):
+    t.bgcolor(next(colors))
     t.pencolor(next(colors))
     t.circle(size)
     t.right(angle)
     t.forward(shift)
     draw_circles(size + 5 , angle + 1, shift + 1)
-t.speed('fastest')
-t.bgcolor('black')
-t.pensize(1)
+t.speed('fast')
+
+t.pensize(40)
 
 draw_circles(5,0,1)
