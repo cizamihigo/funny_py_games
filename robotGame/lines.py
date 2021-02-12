bef10dffacedeb922b8ebbd7f9c12db19497b4b2
cizamihigo <cizamihigochristian@gmail.com>
2021-02-12 12:26:25 +0200

    robot restitution

---
+++ robotGame/lines.py
@@ -0,0 +1,61 @@
+import random
+import turtle as t 
+
+def get_len():
+    choice =input("Enter line length (Long, Medium, Short): ")
+    if choice == 'long':
+        line_len = 200
+    elif choice =='medium':
+        line_len = 100
+    else:
+        line_len = 50
+    return line_len
+def get_wid():
+    choice = input("Enter line width (superthick, thick, thin): ")
+    if choice == 'superthick' :
+        line_wdth = 40
+    elif choice == 'thick':
+        line_wdth = 20
+    else: 
+        line_wdth = 10
+    return line_wdth
+
+def inside_window():
+    leftlimit = (-t.window_width()/2) + 100
+    rightlimit = (t.window_width()/2) - 100
+    top_limit = (t.window_height()/2) - 100
+    bottom_limit = (-t.window_height()/2) + 100
+    (x,y) = t.pos()
+    inside = leftlimit < x < rightlimit and bottom_limit < y < top_limit
+    return inside
+def move(line_len):
+    pen_colors =['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    # t.pencolor(random.choice(pen_colors))
+    t.colormode(255)
+    red = random.randint(0,255)
+    blue = random.randint(0,255)
+    green = random.randint(0,255)
+
+    t.pencolor(red, green, blue)
+    t.fillcolor(random.choice(pen_colors))
+    t.shapesize(3,3,1)
+    t.stamp()
+    if inside_window():
+        angle = random.randint(0,180)
+        t.right(angle)
+        t.forward(line_len)
+    else:
+        t.backward(line_len)
+line_len = get_len()
+line_width = get_wid()
+
+t.shape('turtle')
+t.fillcolor('green')
+t.bgcolor('black')
+t.speed('fastest')
+t.pensize(line_width)
+while True:
+    move(line_len)
+    # t.Screen().mainloop()
+
+
---
+++ robotGame/readme.md
@@ -0,0 +1,10 @@
+### Repo Content 
+> the files contained in this repo are such:
+**turt.py** : a simple turle program to design a robot a simple robot made with python
+**mind-boggling.py** have you used windows XP??? 
+when the PC is spleeping there are such images which were designed on the desktop always starting by simple cercles and appending new cercles one aside others, other inside others and so on till it become complex. the more the program is running the more complicated it becomes.
+>>> EACH NEW CIRCLE IS DIFFERENT FROM THE LAST ONE DRAWN, MAKING THE PATTERN MORE INTERESTING.
+**Night.py** : The night you dreamed about. stars, in the sky, lightning just for you in the night. The longer you
+leave the program running, the more fantastic and colorful the sky becomes.
+**lines.py** : another Windows XP sleep screen implementation/
+this is now using lines and randomly display them till the limit the user entered. this is the only implementation changed from WinXP
