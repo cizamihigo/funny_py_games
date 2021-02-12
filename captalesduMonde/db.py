3f4d20e8b3ce8e34e02387e5a4e6b715ee56bb55
cizamihigo <cizamihigochristian@gmail.com>
2021-02-12 12:47:18 +0200

    from Db to Txt file[C

--- captalesduMonde/db.py
+++ captalesduMonde/db.py
@@ -7,3 +7,18 @@ Ccon =conn.cursor()
 # Ccon.execute("CREATE TABLE CapCountry(Id INTEGER PRIMARY KEY AUTOINCREMENT,\
 #     CountryName VARCHAR(255) UNIQUE,\
 #     CapitalName VARCHAR(200))")
+
+#Ccon.execute("INSERT INTO CapCountry(CountryName, CapitalName) VALUES('India', 'New Dheli'),('China', 'Beijing'),('France', 'Paris'), ('Congo', 'Brazzaville')")
+conn.commit()
+Ccon.execute("SELECT CountryName,CapitalName FROM CapCountry")
+var = Ccon.fetchall()
+write_all = open("captalesduMonde/cap.txt",'w')
+for line in var:
+    print(line)
+    
+    write_all.write(line[0] + ', ' + line[1] +'\n')
+    # write_all.close()
+        
+    # for el in line:
+    #     print(el)
+
