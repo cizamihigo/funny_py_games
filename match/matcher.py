429ac998d2ec15e210a118bdb77183aa93ea756a
cizamihigo <cizamihigochristian@gmail.com>
2021-02-13 17:27:50 +0200

    FINISH

--- match/matcher.py
+++ match/matcher.py
@@ -12,9 +12,9 @@ def show_symbol(x, y):
         first = False
     elif previousX != x or previousY != y:
         if buttons[previousX, previousY]['text'] != buttons[x, y]['text']:
-        time.sleep(0.5)
-        buttons[previousX, previousY]['text'] = ''
-        buttons[x, y]['text'] = ''
+            time.sleep(0.5)
+            buttons[previousX, previousY]['text'] = ''
+            buttons[x, y]['text'] = ''
         else:
             buttons[previousX, previousY]['command'] = DISABLED
             buttons[x, y]['command'] = DISABLED
@@ -32,9 +32,9 @@ symbols = [u'\u2702', u'\u2702', u'\u2705', u'\u2705', u'\u2708', u'\u2708',
  u'\u2714', u'\u2714', u'\u2716', u'\u2716', u'\u2728', u'\u2728']
 random.shuffle(symbols)
 for x in range(6):
- for y in range(4):
- button = Button(command=lambda x=x, y=y: show_symbol(x, y), width=3, height=3)
- button.grid(column=x, row=y)
- buttons[x, y] = button
- button_symbols[x, y] = symbols.pop()
+    for y in range(4):
+        button = Button(command=lambda x=x, y=y: show_symbol(x, y), width=3, height=3)
+        button.grid(column=x, row=y)
+        buttons[x, y] = button
+        button_symbols[x, y] = symbols.pop()
 root.mainloop()
